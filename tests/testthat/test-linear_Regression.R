4af6b65a36a86b831e8a0a684abec16c3b994b0a
ybshao0709 <ybshao@umich.edu>
2021-11-25 22:11:36 -0500

    new change

--- tests/testthat/test-linear_Regression.R
+++ tests/testthat/test-linear_Regression.R
@@ -8,7 +8,7 @@ test_that("linear_Regression works", {
   x <- matrix(c(-1, -2, -3, -4, -5, 1, 3, 5, 7, 9), nrow = 5)
   y <- c(2, 5, 8, 11, 19)
   coefs1 <- as.vector(linear_Regression(y, x, intercept = F)$model.coefs[, 1])
-  coefs2 <- lm(y ~ -1 + x)$coefficients
+  coefs2 <- as.vector(lm(y ~ -1 + x)$coefficients)
   expect_equal(coefs1, coefs2)
 })
 
@@ -16,7 +16,7 @@ test_that("linear_Regression works", {
   x <- matrix(c(-1, -2, -3, -4, -5, 1, 3, 5, 7, 9), nrow = 5)
   y <- c(2, 5, 8, 11, 19)
   coefs1 <- as.vector(linear_Regression(y, x, intercept = F)$model.coefs[, 1])
-  coefs2 <- lm(y ~ -1 + x)$coefficients
+  coefs2 <- as.vector(lm(y ~ -1 + x)$coefficients)
   expect_equal(coefs1, coefs2)
 })
 
@@ -24,6 +24,17 @@ test_that("linear_Regression works", {
   x <- as.matrix(state.x77[,c(1:3, 5:8)])
   y <- state.x77$`Life Exp`
   coefs1 <- as.vector(linear_Regression(y, x)$model.coefs[, 1])
-  coefs2 <- lm(y ~ x)$coefficients
+  coefs2 <- as.vector(lm(y ~ x)$coefficients)
   expect_equal(coefs1, coefs2)
+})
+
+test_that("linear_Regression works", {
+  x <- as.matrix(state.x77[,c(1:3, 5:8)])
+  y <- state.x77$`Life Exp`
+  lower.bound1 <- as.vector(linear_Regression(y, x, CI.beta = T)$model.coefs[, 3])
+  higher.bound1 <- as.vector(linear_Regression(y, x, CI.beta = T)$model.coefs[, 4])
+  lower.bound2 <- as.vector(confint(lm(y ~ x))[,1])
+  higher.bound2 <- as.vector(confint(lm(y ~ x))[,2])
+  expect_equal(lower.bound1, lower.bound2)
+  expect_equal(higher.bound1, higher.bound2)
 })
